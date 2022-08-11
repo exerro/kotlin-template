d5813debe639509c36444e3ebe934739c151bc1e
exerro <benedict.allen2514@gmail.com>
2022-08-11 17:21:19 +0100

    Rename Tests to ExportedFunctionTest

--- src/test/kotlin/me/exerro/ExportedFunctionTest.kt
+++ src/test/kotlin/me/exerro/ExportedFunctionTest.kt
@@ -3,7 +3,7 @@ package me.exerro
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class Tests {
+class ExportedFunctionTest {
     @Test
     fun `exportedFunction returns 42`() {
         assertEquals(42, exportedFunction())
